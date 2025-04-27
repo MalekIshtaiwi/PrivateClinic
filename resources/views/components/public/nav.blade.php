2af6d92d9c594fe677f8eab1d525da9f64585ca6
Malik Ishtaiwe <malekishtiwi3@gmail.com>
2025-04-27 23:22:35 +0300

    updated the structure of the resource to have admin,secretary and public folders and added components folder

---
+++ resources/views/components/public/nav.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">عيادة الشفاء</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">الرئيسية</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">خدماتنا</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">الأطباء</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">حجز موعد</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">تواصل معنا</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
